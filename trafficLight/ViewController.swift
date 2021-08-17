06f2031a25acd58389e5c843e056751ed2e1182a
Kirill Batmanov <BatmanovKS@icloud.com>
2021-08-17 22:30:41 +0200

    Interface settings

--- trafficLight/ViewController.swift
+++ trafficLight/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var orangeLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLightView.alpha = 0.3
+        orangeLightView.alpha = 0.3
+        greenLightView.alpha = 0.3
+        startButton.layer.cornerRadius = 10
+        
     }
 
-
+    @IBAction func startButtonPressed() {
+    }
+    
 }
 
