7e84036416834a2b4f0bb58e49774a524594d2f5
Kirill Batmanov <BatmanovKS@icloud.com>
2021-08-21 18:22:30 +0200

    Final version

--- trafficLight/ViewController.swift
+++ trafficLight/ViewController.swift
@@ -7,25 +7,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum CurrentLight {
+    case red, orange, green
+}
 
+class ViewController: UIViewController {
+    
     @IBOutlet var redLightView: UIView!
     @IBOutlet var orangeLightView: UIView!
     @IBOutlet var greenLightView: UIView!
     
     @IBOutlet var startButton: UIButton!
     
+    private var currentLight = CurrentLight.red
+    private let lightsIsOn: CGFloat = 1
+    private let lightsIsOff: CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        redLightView.alpha = 0.3
-        orangeLightView.alpha = 0.3
-        greenLightView.alpha = 0.3
-        startButton.layer.cornerRadius = 10
         
+        redLightView.alpha = lightsIsOff
+        orangeLightView.alpha = lightsIsOff
+        greenLightView.alpha = lightsIsOff
+        
+        startButton.layer.cornerRadius = 10
     }
-
+    
+    override func viewWillLayoutSubviews() {
+        redLightView.layer.cornerRadius = redLightView.frame.height / 2
+        orangeLightView.layer.cornerRadius = orangeLightView.frame.height / 2
+        greenLightView.layer.cornerRadius = greenLightView.frame.height / 2
+    }
+    
     @IBAction func startButtonPressed() {
+        
+        if startButton.currentTitle == "START" {
+            startButton.setTitle("NEXT", for: .normal)
+        }
+        
+        switch currentLight {
+        case .red:
+            redLightView.alpha = lightsIsOn
+            greenLightView.alpha = lightsIsOff
+            currentLight = .orange
+        case .orange:
+            redLightView.alpha = lightsIsOff
+            orangeLightView.alpha = lightsIsOn
+            currentLight = .green
+        case .green:
+            orangeLightView.alpha = lightsIsOff
+            greenLightView.alpha = lightsIsOn
+            currentLight = .red
+            
+        }
+        
     }
     
 }
-
